8481cb8632c4895f615c9959de1447ac2354c881
Roman Skakun <rm.skakun@gmail.com>
2024-10-28 15:32:30 +0000

    config.h: bold font style
    Signed-off-by: Roman Skakun <rm.skakun@gmail.com>

--- config.def.h
+++ config.def.h
@@ -5,9 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=14:antialias=true:autohint=true";
+static char *font = "Liberation Mono:pixelsize=14:style=Bold:antialias=true:autohint=true";
 static char *font2[] = {
-	"Liberation Mono:pixelsize=12:antialias=true:autohint=true"
+	"Liberation Mono:pixelsize=14:style=Bold:antialias=true:autohint=true"
 };
 static int borderpx = 0;
 
